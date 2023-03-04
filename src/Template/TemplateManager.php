c1ca2e6a830f31415a9eec7730e3e4a94607b9f0
CraftyDigit <oleinik.v89@gmail.com>
2023-03-05 00:38:53 +0600

    fix: add throws exception docblock to Template\TemplateManager method

--- src/Template/TemplateManager.php
+++ src/Template/TemplateManager.php
@@ -2,12 +2,15 @@
 
 namespace CraftyDigit\Puff\Template;
 
+use Exception;
+
 class TemplateManager implements TemplateManagerInterface
 {
     /**
      * @param string $templateName
      * @param bool $isAdminTemplate
      * @return TemplateInterface
+     * @throws Exception
      */
     public function getTemplate(string $templateName, bool $isAdminTemplate = false): TemplateInterface
     {
