da3c808e60cca43bc52bd5822827126b4cf277bb
CraftyDigit <oleinik.v89@gmail.com>
2023-04-24 12:40:54 +0600

    feat: AbstractDataController
    From now we can use the "AbstractDataController" class when we need a default "EntityManager" out of the box, and the"AbstractController" in other cases.

--- src/Controller/AbstractController.php
+++ src/Controller/AbstractController.php
@@ -3,28 +3,22 @@
 namespace CraftyDigit\Puff\Controller;
 
 use CraftyDigit\Puff\Config\Config;
-use CraftyDigit\Puff\DataHandler\DataHandlerInterface;
-use CraftyDigit\Puff\EntityManager\NoStruct\NoStructEntityManagerInterface;
-use CraftyDigit\Puff\Enums\DataSourceType;
+use CraftyDigit\Puff\Container\ContainerExtendedInterface;
 use CraftyDigit\Puff\Enums\TemplateEngine;
 use CraftyDigit\Puff\Template\TemplateEngineInterface;
 use CraftyDigit\Puff\Template\TemplateEngineManagerInterface;
-use Doctrine\ORM\EntityManagerInterface;
 use Exception;
 
 abstract class AbstractController
 {
     public function __construct(
+        protected ContainerExtendedInterface $container,
         protected readonly TemplateEngineManagerInterface $templateEngineManager,
-        protected readonly DataHandlerInterface $dataHandler,
         protected readonly Config $config,
         protected ?TemplateEngineInterface $templateEngine = null,
-        protected NoStructEntityManagerInterface|EntityManagerInterface|null $entityManager = null
     )
     {
         $this->setTemplateEngine();
-
-        $this->setEntityManager();
     }
 
     private function setTemplateEngine(): void
@@ -36,16 +30,5 @@ abstract class AbstractController
         }
 
         $this->templateEngine = $this->templateEngineManager->getTemplateEngine($defaultEngine);
-    }
-
-    private function setEntityManager(): void
-    {
-        $defaultEntityManager = DataSourceType::tryFrom($this->config->default_data_handler);
-
-        if ($defaultEntityManager === null) {
-            throw new Exception('Invalid default entity manager');
-        }
-
-        $this->entityManager = $this->dataHandler->getEntityManager($defaultEntityManager);
     }
 }
