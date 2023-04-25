a331b6c6cb5ce5527ccd9c047107c3e4b419aa95
CraftyDigit <oleinik.v89@gmail.com>
2023-04-25 22:48:20 +0600

    feat: Doctrine caching

--- src/DataHandler/DataHandler.php
+++ src/DataHandler/DataHandler.php
@@ -10,9 +10,12 @@ use CraftyDigit\Puff\Enums\DataSourceType;
 use CraftyDigit\Puff\Exceptions\ClassNotFoundException;
 use CraftyDigit\Puff\Helper;
 use Doctrine\DBAL\DriverManager;
+use Doctrine\ORM\Cache\DefaultCacheFactory;
+use Doctrine\ORM\Cache\RegionsConfiguration;
 use Doctrine\ORM\EntityManager;
 use Doctrine\ORM\EntityManagerInterface;
 use Doctrine\ORM\ORMSetup;
+use Symfony\Component\Cache\Adapter\FilesystemAdapter;
 
 readonly class DataHandler implements DataHandlerInterface
 {
@@ -31,10 +34,41 @@ readonly class DataHandler implements DataHandlerInterface
                 break;
                 
             case DataSourceType::DOCTRINE :
+                $doctrineConfig = $this->config->doctrine;
+
                 $connectConfig = ORMSetup::createAttributeMetadataConfiguration(
                     paths: [$this->helper->getPathToSrcSubDirectory('Entities')],
                     isDevMode: $this->config->mode === 'dev',
                 );
+                
+                if ($doctrineConfig['cache_enabled']) {
+                    /** @var FilesystemAdapter $cache */
+                    $cache = $this->container->get(
+                        FilesystemAdapter::class,
+                        [
+                            'defaultLifetime' => $doctrineConfig['cache_lifetime'],
+                            'directory' => $this->helper->getPathToAppSubDirectory($doctrineConfig['cache_dir'])
+                        ]
+                    );
+
+                    $connectConfig->setQueryCache($cache);
+                    $connectConfig->setResultCache($cache);
+
+                    /** @var RegionsConfiguration $cache2LConfig */
+                    $cache2LConfig = $this->container->get(
+                        RegionsConfiguration::class, 
+                        ['defaultLifetime' => $doctrineConfig['cache_lifetime']]
+                    );
+
+                    /** @var DefaultCacheFactory $cacheFactory */
+                    $cacheFactory = $this->container->get(DefaultCacheFactory::class, [$cache2LConfig, $cache]);
+
+                    // Enable second-level-cache
+                    $connectConfig->setSecondLevelCacheEnabled();
+                    // Cache factory
+                    $connectConfig->getSecondLevelCacheConfiguration()
+                        ->setCacheFactory($cacheFactory);    
+                }
 
                 $dbConfig = $this->config->db;
 
