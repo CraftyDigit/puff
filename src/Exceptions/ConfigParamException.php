a427fbb2a00535dc75522a3c3eeaf28555adaa4f
CraftyDigit <oleinik.v89@gmail.com>
2023-05-05 12:52:59 +0600

    feat: ConfigParamException

---
+++ src/Exceptions/ConfigParamException.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace CraftyDigit\Puff\Exceptions;
+
+class ConfigParamException extends \Exception
+{
+    public function __construct(
+        $message = "", $code = 404, 
+        Throwable $previous = null, 
+        ?string $configParamName = null, 
+        ?string $configParamValue = null
+    )
+    {
+        if ($message === "") {
+            if ($configParamName === null) {
+                $message = 'Config param not found or invalid.';
+            } else if (!$configParamValue) { 
+                $message = sprintf('Config param %s not found or invalid', $configParamName);
+            } else {
+                $message = sprintf('Config param %s not found or it\'s value "%s" is invalid', $configParamName, $configParamValue);
+            }
+        }
+
+        parent::__construct($message, $code, $previous);
+    }    
+}
